bcb11818f01fcbfc1c558def0cebcb46adb445e4
wukunyun <864353360@qq.com>
2022-06-22 22:47:09 +0800

    const

--- hello.go
+++ hello.go
@@ -2,28 +2,37 @@ package main
 
 import "fmt"
 
-var c = 222
+//var c = 222
 
-var (
-	xx = 1
-	yy = 1
-	zz = 1
-)
+//var (
+//	xx int = 1
+//	yy int = 1
+//	zz     = 1
+//)
 
-func variable() {
-	var a, b int = 1, 2
-	var s string = "Hello World"
-	fmt.Println(a, s, b)
-}
+//func variable() {
+//	var a, b int = 1, 2
+//	var s string = "Hello World"
+//	fmt.Println(a, s, b)
+//}
+//
+//func variableTypeDeduction() {
+//	a, b := 1, 2
+//	//a = b + 2
+//	fmt.Println(a, b, c)
+//	fmt.Println(xx, yy, zz)
+//}
 
-func variableTypeDeduction() {
-	a, b := 1, 2
-	//a = b + 2
-	fmt.Println(a, b, c)
-	fmt.Println(xx, yy, zz)
+func constTest() {
+	const a = 1
+	fmt.Println(a)
+	{
+		const a = 2
+		fmt.Println(a)
+	}
 }
 
 func main() {
 	//variable()
-	variableTypeDeduction()
+	constTest()
 }
