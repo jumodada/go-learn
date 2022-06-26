9e4d73c0ec960a1c7c45759948dff6e8250feefa
wukunyun <864353360@qq.com>
2022-06-26 15:19:18 +0800

    rune and byte

--- map.go
+++ map.go
@@ -3,10 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	m1 := map[string]int{
-		"ss": 1,
+	m1 := map[string]string{
+		"luna": "moon",
 	}
-	for k, v := range m1 {
-		fmt.Println(k, v)
-	}
+	//for k, v := range m1 {
+	//	fmt.Println(k, v)
+	//}
+	delete(m1, "luna")
+	val, hasLion := m1["lion"]
+	fmt.Println(val, hasLion)
+	fmt.Println(m1["luna"])
 }
---
+++ string.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	s := "干了兄弟萌"
+	//fmt.Printf("%d ", s)
+	//fmt.Println(utf8.RuneCountInString(s))
+	//bytes := []byte(s)
+	//for len(bytes) > 0 {
+	//	_, size := utf8.DecodeRune(bytes)
+	//	fmt.Println("index: ", size)
+	//	bytes = bytes[size:]
+	//	fmt.Println(bytes)
+	//}
+	//for i, ch := range []rune(s) {
+	//	fmt.Println(i, ch)
+	//}
+	//for i, ch := range []byte(s) {
+	//	fmt.Println(i, ch)
+	//}
+	res := []rune(s)
+	fmt.Println(string(res[1:]))
+}
