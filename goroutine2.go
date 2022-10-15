b8ed065479cec33bb4ad942ecb273efcff2a0235
xipper <864353360@qq.com>
2022-10-15 12:56:40 +0800

    Create goroutine2.go

---
+++ goroutine2.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func hello(i int) {
+	defer wg.Done()
+	fmt.Println(i)
+}
+func main() {
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go hello(i)
+	}
+	wg.Wait()
+}
